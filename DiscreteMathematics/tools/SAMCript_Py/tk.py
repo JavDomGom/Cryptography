4fb042bc03faee6687c5235353dd44eff3fb927b
JavierDominguezGomez <jdomgo3@gmail.com>
2020-01-15 19:47:45 +0100

    Fix operations combobox.

--- DiscreteMathematics/tools/SAMCript_Py/tk.py
+++ DiscreteMathematics/tools/SAMCript_Py/tk.py
@@ -1,6 +1,6 @@
-from os import getcwd
-from tkinter import *
-from tkinter.ttk import Combobox
+# from os import getcwd
+from tkinter import Tk, IntVar, StringVar, Frame, Label, Entry, Button, Menu
+from tkinter.ttk import Combobox, Style
 from tkinter import messagebox as MessageBox
 
 program_name = 'SAMCript_py'
@@ -10,28 +10,35 @@ pady = 3
 font = ('Courier New', 10)
 
 
-def set_base():
-    selected_base = cb_base.get()
-    if selected_base == cb_base_op_base2:
+def set_base(event):
+    b = cb_base.get()
+
+    if b == cb_base_op_base2:
         base.set(2)
-    elif selected_base == cb_base_op_base10:
+    elif b == cb_base_op_base10:
         base.set(10)
-    elif selected_base == cb_base_op_base16:
+    elif b == cb_base_op_base16:
         base.set(16)
+
+    print(f'base = {base.get()}')
+
+
+def set_operation(event):
+    operation.set(cb_operation.get())
+    print(f'operation = {operation.get()}')
 
 
 def calculate():
-    selected_operation = cb_operation.get()
-    if selected_operation == cb_operation_op_default:
-        operation.set()
-    elif selected_operation == cb_operation_op_addition:
-        operation.set()
-    elif selected_operation == cb_operation_op_substraction:
-        operation.set()
-    elif selected_operation == cb_operation_op_multiplication:
-        operation.set()
-    elif selected_operation == cb_operation_op_division:
-        operation.set()
+    op = cb_operation.get()
+
+    if op == cb_operation_op_addition:
+        addition()
+    elif op == cb_operation_op_substraction:
+        substraction()
+    elif op == cb_operation_op_multiplication:
+        multiplication()
+    elif op == cb_operation_op_division:
+        division()
 
 
 def addition():
@@ -73,8 +80,20 @@ def about():
 root = Tk()
 root.geometry('650x350')
 root.title(f'{program_name} - {programa_description}')
-root.resizable(0, 0)
+root.resizable(1, 0)
 # root.iconbitmap(f'@{getcwd()}/img/icon.xbm')
+
+style = Style()
+style.theme_create('custom_style',
+                   parent='default',
+                   settings={'TCombobox':
+                             {'configure':
+                              {'selectforeground': 'black',
+                               'selectbackground': 'white'}
+                              }
+                             }
+                   )
+style.theme_use('custom_style')
 
 menubar = Menu(root)
 root.config(menu=menubar)
@@ -108,7 +127,7 @@ frame1.config(cursor='arrow', bg='CadetBlue1', bd=5)
 
 base = IntVar()
 
-cb_base_op_default = 'Please select base'
+cb_base_op_default = '-- Select base --'
 cb_base_op_base2 = 'Binary (Base-2)'
 cb_base_op_base10 = 'Decimal (Base-10)'
 cb_base_op_base16 = 'Hexadecimal (Base-16)'
@@ -120,13 +139,13 @@ cb_base['values'] = [
     cb_base_op_base10,
     cb_base_op_base16
 ]
-cb_base.current(newindex=0)
+cb_base.current(0)
 cb_base.bind('<<ComboboxSelected>>', set_base)
-cb_base.grid(row=0, column=0, sticky='w')
+cb_base.grid(row=0, column=0, padx=padx, pady=pady, sticky='w')
 
 operation = StringVar()
 
-cb_operation_op_default = 'Please select operation'
+cb_operation_op_default = '-- Select operation --'
 cb_operation_op_addition = 'Addition'
 cb_operation_op_substraction = 'Substraction'
 cb_operation_op_multiplication = 'Multiplication'
@@ -138,11 +157,11 @@ cb_operation['values'] = [
     cb_operation_op_addition,
     cb_operation_op_substraction,
     cb_operation_op_multiplication,
-    cb_operation_op_division
+    cb_operation_op_division,
 ]
-cb_operation.current(newindex=0)
+cb_operation.current(0)
 cb_operation.bind('<<ComboboxSelected>>', set_operation)
-cb_operation.grid(row=0, column=1, sticky='w')
+cb_operation.grid(row=0, column=1, padx=padx, pady=pady, sticky='w')
 
 
 frame2 = Frame(root)
@@ -152,6 +171,8 @@ frame2.config(cursor='arrow', bd=5)
 op1 = StringVar()
 op2 = StringVar()
 res = StringVar()
+
+root.grid_columnconfigure(1, weight=0)
 
 label1 = Label(frame2, text='First number')
 label1.grid(row=0, column=0, padx=padx, pady=pady, sticky='e')
@@ -168,20 +189,7 @@ label3.grid(row=2, column=0, padx=padx, pady=pady, sticky='e')
 entry3 = Entry(frame2, font=font, textvariable=res, state='readonly')
 entry3.grid(row=2, column=1, padx=padx, pady=pady)
 
-
-frame4 = Frame(root)
-frame4.pack(fill='both', expand=1)
-frame4.config(cursor='arrow', bg='lightgreen', bd=5)
-
-# button1 = Button(frame4, text='Addition', command=addition)
-# button1.grid(row=0, column=0)
-# button2 = Button(frame4, text='Substraction', command=substraction)
-# button2.grid(row=0, column=1)
-# button3 = Button(frame4, text='Multiplication', command=multiplication)
-# button3.grid(row=0, column=2)
-# button4 = Button(frame4, text='Division', command=division)
-# button4.grid(row=0, column=3)
-button1 = Button(frame4, text='Calculate', command=addition)
-button1.grid(row=0, column=0)
+button = Button(frame2, text='Calulate', command=calculate)
+button.grid(row=3, column=1, padx=padx, pady=pady, sticky='w')
 
 root.mainloop()
